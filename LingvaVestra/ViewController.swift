2f1a1b8269bbf411e4b1e7c2ffd405bce6025d38
Sergey Pavlov <mosevaly@gmail.com>
2023-12-06 14:59:06 +0300

    Add Snapkit, Swiftlint script + swiftlint.yml

--- LingvaVestra/ViewController.swift
+++ LingvaVestra/ViewController.swift
@@ -9,11 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // swiftlint:disable:next unneeded_override
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
-
