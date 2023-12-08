b5088d65460b9360ba14a826c55461de3bf9599d
Sergey Pavlov <mosevaly@gmail.com>
2023-12-08 14:54:19 +0300

    Add NavigationManager, add AuthViewController

---
+++ LingvaVestra/AuthViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AuthViewController.swift
+//  LingvaVestra
+//
+//  Created by Sergey Pavlov on 08.12.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        title = "Login screen"
+    }
+}
---
+++ LingvaVestra/NavigationManager.swift
@@ -0,0 +1,60 @@
+//
+//  NavigationManager.swift
+//  LingvaVestra
+//
+//  Created by Sergey Pavlov on 08.12.2023.
+//
+
+import UIKit
+
+enum Route {
+    case auth
+    case tabBar
+}
+
+final class NavigationManager {
+
+    static let shared = NavigationManager()
+    private var window: UIWindow?
+
+    private init() {}
+
+    /// Sets the main window for the app.
+    func setWindow(_ window: UIWindow?) {
+        self.window = window
+    }
+
+    /// Navigates to the specified route.
+    ///
+    /// - Parameter route: The route to navigate to.
+    ///     - If the route is  `.auth`, sets the AuthViewController as the root.
+    ///     - If the route is `.tabBar`, sets the TabBarController as the root.
+    func navigate(_ route: Route) {
+        switch route {
+        case .auth:
+            setRootController(createAuthViewController())
+        case .tabBar:
+            setRootController(createTabBarController())
+        }
+    }
+}
+// MARK: - Private methods
+
+private extension NavigationManager {
+
+    func setRootController(_ controller: UIViewController) {
+        guard let window = window else { return }
+        window.rootViewController = controller
+        window.makeKeyAndVisible()
+    }
+
+    func createAuthViewController() -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: AuthViewController())
+        return navigationController
+    }
+
+    func createTabBarController() -> UITabBarController {
+        // change later: return BaseTabBarController
+        UITabBarController()
+    }
+}
